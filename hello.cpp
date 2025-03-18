253db79e6f4921013383da969befecc93e6f65f1
rrrox007 <131610965+rrrox007@users.noreply.github.com>
2025-03-18 16:35:01 +0530

    PES1UG22CS488

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "hi this is from webhook \n"
-  cout << "Bro TA make a better a lab this is too hard to understand \n"
+  cout << "hi this is from webhook \n";
+  cout << "Bro TA make a better a lab this is too hard to understand \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
