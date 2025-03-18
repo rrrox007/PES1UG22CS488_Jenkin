61593288e4b8a035e7d16f1bfa917404d1f1881e
rrrox007 <131610965+rrrox007@users.noreply.github.com>
2025-03-18 23:20:04 +0530

    PES1UG22CS488

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "hi this is from webhook \n";
-  cout << "Bro TA make a better a lab this is too hard to understand \n";
+  cout << "HI TA , How are you? \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
